ee6165400c22ad84d883eaf327f231385ac078be
Wyn Zhang <wyn@roadhop.com>
2013-12-18 15:32:30 +0800

    Added ssl configuration option for nginx.

--- lib/capistrano-zen/nginx.rb
+++ lib/capistrano-zen/nginx.rb
@@ -5,6 +5,16 @@ configuration = Capistrano::Configuration.respond_to?(:instance) ?
   Capistrano.configuration(:must_exist)
 
 configuration.load do
+  _cset(:nginx_use_ssl, false)
+
+  if nginx_use_ssl
+    _cset(:nginx_ssl_certificate) { "#{domain}.crt" }
+    _cset(:nginx_ssl_certificate_key) { "#{domain}.key" }
+    _cset(:nginx_upload_local_ssl_certificate) { true }
+    _cset(:nginx_ssl_certificate_path) { "#{config_path}/ssl/#{nginx_ssl_certificate}" }
+    _cset(:nginx_ssl_certificate_key_path) { "#{config_path}/ssl/#{nginx_ssl_certificate_key}"}
+  end
+
   namespace :nginx do
     desc "Install latest stable release of nginx"
     task :install, roles: :web do
@@ -21,6 +31,20 @@ configuration.load do
         template "nginx_unicorn.erb", "/tmp/nginx_conf"
         run "#{sudo} mv /tmp/nginx_conf /etc/nginx/sites-enabled/#{application}"
         run "#{sudo} rm -f /etc/nginx/sites-enabled/default"
+
+        if nginx_use_ssl
+          if nginx_upload_local_ssl_certificate
+            put File.read(nginx_ssl_certificate_path), "/tmp/#{nginx_ssl_certificate}"
+            put File.read(nginx_ssl_certificate_key_path), "/tmp/#{nginx_ssl_certificate_key}"
+
+            run "#{sudo} mv /tmp/#{nginx_ssl_certificate} /etc/ssl/#{nginx_ssl_certificate}"
+            run "#{sudo} mv /tmp/#{nginx_ssl_certificate_key} /etc/ssl/#{nginx_ssl_certificate_key}"
+          end
+
+          run "#{sudo} chown root:root /etc/ssl/#{nginx_ssl_certificate}"
+          run "#{sudo} chown root:root /etc/ssl/#{nginx_ssl_certificate_key}"
+        end
+
         restart
       end
 
